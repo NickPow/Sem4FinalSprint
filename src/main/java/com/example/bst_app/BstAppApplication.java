873fab2c23d53596bc93b682f6b92f1a228d858a
Nicholas Power <nicholas-power@outlook.com>
2025-08-12 12:53:55 -0230

    Commit 1 - Initial Spring Boot

---
+++ src/main/java/com/example/bst_app/BstAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bst_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bst_app/BstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bst_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
